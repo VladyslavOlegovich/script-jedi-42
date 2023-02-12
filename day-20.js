bcf934a7490582564f54d528a2a9a7c9f3760a0e
Vladyslav <vladionkin@icloud.com>
2023-02-12 21:02:45 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139
+const regex = /^(-9|9)\d*0{4}$/;
+
+// http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+const regex = /(http:\/\/|https:\/\/)[a-z0-9.]+(\.com|\.net)/gi;

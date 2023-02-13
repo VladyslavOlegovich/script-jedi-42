9648de3d9fff6f65d602c6450c1c56380172b730
Vladyslav <vladionkin@icloud.com>
2023-02-13 23:23:26 +0200

    Create day-22.js

---
+++ day-22.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
+const anyArrows = (arrows) => arrows.some((arrow) => !arrow.damaged);
+
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+const multiplyAll = (arr) => (num) => arr.map((el) => el * num);

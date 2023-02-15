1d25793dc398352b5cbfbe733eca593e504f8794
Vladyslav <vladionkin@icloud.com>
2023-02-15 23:15:09 +0200

    Create day-25.js

---
+++ day-25.js
@@ -0,0 +1,12 @@
+// http://www.codewars.com/kata/anonymous-returns
+const name = 'The Window';
+
+const alpha = {
+  name: 'My Alpha',
+  getNameFunc() {
+    return () => this.name;
+  },
+};
+
+// https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+const ArrowFunc = (arr) => arr.map((el) => String.fromCharCode(el)).join('');

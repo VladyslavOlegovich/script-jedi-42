4283991c7c6d1f4946394c35a77312c788d520b4
Vladyslav <vladionkin@icloud.com>
2023-02-24 16:35:09 +0200

    Create day-35.js

---
+++ day-35.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
+Object.prototype.description = 'TODO: GIVE ME A DESCRIPTION';
+
+// https://www.codewars.com/kata/javascript-class-like-objects
+class Animal {
+  constructor(name, type) {
+    this._name = name;
+    this._type = type;
+  }
+  toString() {
+    return this._name + ' is a ' + this._type;
+  }
+  get name() {
+    return this._name;
+  }
+  get type() {
+    return this._type;
+  }
+  set name(name) {
+    this._name = name;
+  }
+}
+
+// http://www.codewars.com/kata/power-bind
+Function.prototype.bind = function(context) {
+  const func = this;
+  return function(...args) {
+    const rightContext = this === global ? context : this;
+    return func.apply(rightContext, args);
+  };
+};

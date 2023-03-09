109e720bdac706571829686d926cc72ad5902491
Vladyslav <vladionkin@icloud.com>
2023-03-09 22:25:47 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': ['eslint:recommended', 'google'],
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+  extends: ['eslint:recommended', 'google'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'rules': {
-    'camelcase': 'error',
+  rules: {
+    camelcase: 'error',
     'valid-jsdoc': 0,
-    'eqeqeq': 'error'
+    eqeqeq: 'error',
   },
+  // eslint-disable-next-line no-dupe-keys
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
 };
+

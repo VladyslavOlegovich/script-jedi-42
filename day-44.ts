bdfce1bedf9380f1f79ba07d98378dfe1c328946
Vladyslav <vladionkin@icloud.com>
2023-03-12 23:20:33 +0200

    Create day-44.ts

---
+++ day-44.ts
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/591305520c6f9f5c93000052
+interface IGeometricFigure {
+    area: () => number;
+    perimeter: () => number;
+  }
+  
+  export class Square implements IGeometricFigure {
+    constructor(private number: number){}
+      area() {
+      return Math.pow(this.number, 2);
+    } 
+    perimeter() {
+      return this.number * 4;
+    }
+  }
+  
+  export class Circle implements IGeometricFigure {
+    constructor(private radius:number) {}
+    area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+    perimeter() {
+      return 2 * Math.PI * this.radius;
+    }
+  }
+
+  // https://www.codewars.com/kata/590cb67bc69e3a31d0000042
+  
+  // :(
